6961b489177e464d43b4c7241218dde7f993a60f
Le Trung Hau <leetrunghaau@gmail.com>
2024-12-18 18:11:22 +0700

    role map

---
+++ src/store/permissionsCode.js
@@ -0,0 +1,58 @@
+const permissionsCode = {
+    PROJECT: {
+        UPDATE: "BBC",
+        CLOSE: "BBC",
+        MEMBER_NAMAGER: "BBC",
+        ROLE_MANAGER: "BBC",
+        DELETE: "BBC",
+    },
+    ISSUCE: {
+        CREATE:"ABC",
+        DELETE:{
+            ANY: "ABC",
+            OWN: "ABC",
+            ASSIGNEE: "ABC"
+        },
+        CLOSE:{
+            ANY: "ABC",
+            OWN: "ABC",
+            ASSIGNEE: "ABC"
+        },
+        STATUS:{
+            ANY: "ABC",
+            OWN: "ABC",
+            ASSIGNEE: "ABC"
+        },
+        PRIORITY:{
+            ANY: "ABC",
+            OWN: "ABC",
+            ASSIGNEE: "ABC"
+        },
+        TRACTER:{
+            ANY: "ABC",
+            OWN: "ABC",
+            ASSIGNEE: "ABC"
+        },
+        UPDATE:{
+            ANY: "ABC",
+            OWN: "ABC",
+            ASSIGNEE: "ABC"
+        },
+        NOTE: {
+            ANY: "ABC",
+            OWN: "ABC",
+            ASSIGNEE: "ABC"
+        },
+        CHECK_LIST: {
+            ANY: "ABC",
+            OWN: "ABC",
+            ASSIGNEE: "ABC"
+        },
+        ASSIGNEE:{
+            ANY: "ABC",
+            OWN: "ABC",
+        },
+    }
+}
+
+export default permissionsCode
